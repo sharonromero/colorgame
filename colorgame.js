a34b6b2ac53e47f7d0bb1cb27bb5b43918478ce3
sharonromero <sh-aron@hotmail.com>
2019-03-26 16:51:37 -0600

    Cleaned up JS so it's not so long

--- colorgame.js
+++ colorgame.js
@@ -6,54 +6,92 @@ var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
-var easyBtn = document.querySelector("#easyBtn");
-var hardBtn = document.querySelector("#hardBtn");
+var modeButtons = document.querySelectorAll(".mode");
 
+for(var i = 0; i < modeButtons.length; i++){
+	modeButtons[i].addEventListener("click", function(){
+		modeButtons[0].classList.remove("selected");
+		modeButtons[1].classList.remove("selected");
+		this.classList.add("selected");	
 
-easyBtn.addEventListener("click", function(){
-	hardBtn.classList.remove("selected");
-	easyBtn.classList.add("selected");
-	numSquares = 3;
-	colors = generateRandomColors(numSquares);
-	pickedColor = pickColor();
-	colorDisplay.textContent = pickedColor;
-	for(var i = 0; i < squares.length; i++){
-		if(colors[i]){
-			squares[i].style.backgroundColor = colors[i];
-		} else {
-			squares[i].style.display = "none";
-		}
-	}	
-});
+		this.textContent === "Easy" ? numSquares = 3: numSquares = 6; // This line does the same thing as lines 18-22.
+		// if(this.textContent === "Easy"){
+		// 	numSquares = 3;
+		// } else {
+		// 	numSquares = 6;
+		// }
+		reset();
+	});	
+}
 
-hardBtn.addEventListener("click", function(){
-	hardBtn.classList.add("selected");
-	easyBtn.classList.remove("selected");
-	numSquares = 6;
-	colors = generateRandomColors(numSquares);
-	pickedColor = pickColor();
-	colorDisplay.textContent = pickedColor;
-	for(var i = 0; i < squares.length; i++){
-			squares[i].style.backgroundColor = colors[i];
-			squares[i].style.display = "block";
-	}		
-});
-
-resetButton.addEventListener("click", function(){
-	// Generate all new colors
+function reset(){
 	colors = generateRandomColors(numSquares);
 	// Pick a new random color from array
 	pickedColor = pickColor();
 	// Change colorDisplay to match picked color
 	colorDisplay.textContent = pickedColor;
-	this.textContent = "New Colors"; // Makes New Colors button appear again after game is finished and Play Again button is clicked.
+	resetButton.textContent = "New Colors"; // Makes New Colors button appear again after game is finished and Play Again button is clicked.
 	messageDisplay.textContent = ""; // Removes the word Correct when the game is over and the Play Again button is clicked.
 	// Change colors of squares
 	for(var i = 0; i < squares.length; i++){
-		// Adds initial colors to squares
-		squares[i].style.backgroundColor = colors[i];
+		if(colors[i]){
+			squares[i].style.display = "block";
+			squares[i].style.backgroundColor = colors[i];
+		} else {
+			squares[i].style.display = "none";
+		}	
 	}
 	h1.style.backgroundColor = "steelblue";
+}
+
+// Replaced the code below with the code above to shorten the code overall. 
+// easyBtn.addEventListener("click", function(){
+// 	hardBtn.classList.remove("selected");
+// 	easyBtn.classList.add("selected");
+// 	numSquares = 3;
+// 	colors = generateRandomColors(numSquares);
+// 	pickedColor = pickColor();
+// 	colorDisplay.textContent = pickedColor;
+// 	for(var i = 0; i < squares.length; i++){
+// 		if(colors[i]){
+// 			squares[i].style.backgroundColor = colors[i];
+// 		} else {
+// 			squares[i].style.display = "none";
+// 		}
+// 	}	
+// });
+
+// hardBtn.addEventListener("click", function(){
+// 	hardBtn.classList.add("selected");
+// 	easyBtn.classList.remove("selected");
+// 	numSquares = 6;
+// 	colors = generateRandomColors(numSquares);
+// 	pickedColor = pickColor();
+// 	colorDisplay.textContent = pickedColor;
+// 	for(var i = 0; i < squares.length; i++){
+// 			squares[i].style.backgroundColor = colors[i];
+// 			squares[i].style.display = "block";
+// 	}		
+// });
+
+resetButton.addEventListener("click", function(){
+	reset();
+
+	// Since we changed the code above to make it shorter, we can now just use reset(); instead of all of the commented out code below.
+	// Generate all new colors
+	// colors = generateRandomColors(numSquares);
+	// Pick a new random color from array
+	// pickedColor = pickColor();
+	// Change colorDisplay to match picked color
+	// colorDisplay.textContent = pickedColor;
+	// this.textContent = "New Colors"; // Makes New Colors button appear again after game is finished and Play Again button is clicked.
+	// messageDisplay.textContent = ""; // Removes the word Correct when the game is over and the Play Again button is clicked.
+	// Change colors of squares
+	// for(var i = 0; i < squares.length; i++){
+	// 	Adds initial colors to squares
+	// 	squares[i].style.backgroundColor = colors[i];
+	// }
+	// h1.style.backgroundColor = "steelblue";
 })
 
 colorDisplay.textContent = pickedColor; // This displays the goal color in the h1.
