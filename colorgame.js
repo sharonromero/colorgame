d8d863d5dc490a1d416ecf7afdd4e5d788bc4032
sharonromero <sh-aron@hotmail.com>
2019-03-26 14:37:15 -0600

    Added JS so three colors display with easy button and three colors display when play again is chosed while in easy mode

--- colorgame.js
+++ colorgame.js
@@ -1,5 +1,5 @@
-var colors = generateRandomColors(6);
-
+var numSquares = 6;
+var colors = generateRandomColors(numSquares);
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor(); // This picks the goal color (the color that is to be guessed).
 var colorDisplay = document.getElementById("colorDisplay");
@@ -9,20 +9,39 @@ var resetButton = document.querySelector("#reset");
 var easyBtn = document.querySelector("#easyBtn");
 var hardBtn = document.querySelector("#hardBtn");
 
+
 easyBtn.addEventListener("click", function(){
 	hardBtn.classList.remove("selected");
-	easyBtn.classList.add("selected");	
+	easyBtn.classList.add("selected");
+	numSquares = 3;
+	colors = generateRandomColors(numSquares);
+	pickedColor = pickColor();
+	colorDisplay.textContent = pickedColor;
+	for(var i = 0; i < squares.length; i++){
+		if(colors[i]){
+			squares[i].style.backgroundColor = colors[i];
+		} else {
+			squares[i].style.display = "none";
+		}
+	}	
 });
 
 hardBtn.addEventListener("click", function(){
 	hardBtn.classList.add("selected");
-	easyBtn.classList.remove("selected");	
+	easyBtn.classList.remove("selected");
+	numSquares = 6;
+	colors = generateRandomColors(numSquares);
+	pickedColor = pickColor();
+	colorDisplay.textContent = pickedColor;
+	for(var i = 0; i < squares.length; i++){
+			squares[i].style.backgroundColor = colors[i];
+			squares[i].style.display = "block";
+	}		
 });
-
 
 resetButton.addEventListener("click", function(){
 	// Generate all new colors
-	colors = generateRandomColors(6);
+	colors = generateRandomColors(numSquares);
 	// Pick a new random color from array
 	pickedColor = pickColor();
 	// Change colorDisplay to match picked color
