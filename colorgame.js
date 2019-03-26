422307970b416d31f8c73d16ff11e383e9465aad
sharonromero <sh-aron@hotmail.com>
2019-03-26 13:28:10 -0600

    Finished function to randomly choose a goal color, added JS to change h1 to same color as goal color when goal color is chosen correctly

--- colorgame.js
+++ colorgame.js
@@ -1,16 +1,10 @@
-var colors = [
-	"rgb(255, 0, 0)",
-	"rgb(255, 255, 0)",
-	"rgb(0, 255, 0)",
-	"rgb(0, 255, 255)",
-	"rgb(0, 0, 255)",
-	"rgb(255, 0, 255)",
-]
+var colors = generateRandomColors(6);
 
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor(); // This picks the goal color (the color that is to be guessed).
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
 
 colorDisplay.textContent = pickedColor; // This displays the goal color in the h1.
 
@@ -27,6 +21,7 @@ for(var i = 0; i < squares.length; i++){
 		if (clickedColor === pickedColor){
 			messageDisplay.textContent = "Correct!";
 			changeColors(clickedColor);
+			h1.style.backgroundColor = clickedColor;
 		} else {
 			this.style.backgroundColor = "#232323";
 			messageDisplay.textContent = "Try Again"
@@ -45,4 +40,26 @@ function changeColors(color){
 function pickColor(){
 	var random = Math.floor(Math.random() * colors.length);
 	return colors[random];
+}
+
+function generateRandomColors(num){
+	// Make an array
+	var arr = []
+	// Repeat num times
+	for(var i = 0; i < num; i++){
+		// Get random color and push into array
+		arr.push(randomColor())
+	}
+	// Return the array
+	return arr;
+}
+
+function randomColor(){
+	// Pick a "red" from 0-255
+	var r = Math.floor(Math.random() * 256);
+	// Pick a "green" from 0-255
+	var g = Math.floor(Math.random() * 256);
+	// Pick a "blue" from 0-255
+	var b = Math.floor(Math.random() * 256);
+	return "rgb(" + r + ", " + g + ", " + b + ")"; // Spaces must be after the commas or else we won't ever get a correct color.
 }
